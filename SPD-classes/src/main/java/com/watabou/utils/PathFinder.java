16df84fedf69d41a9b4f24ce2c617e262d7d18da
juh9870 <juh9870@ukr.net>
2021-06-09 21:01:49 +0300

    Fixed PathFinder

--- SPD-classes/src/main/java/com/watabou/utils/PathFinder.java
+++ SPD-classes/src/main/java/com/watabou/utils/PathFinder.java
@@ -67,6 +67,8 @@ public class PathFinder {
 
 		NEIGHBOURS4 = new int[]{-width, -1, +1, +width};
 		NEIGHBOURS5 = new int[]{-width, -1, 0, +1, +width};
+		NEIGHBOURS8 = new int[]{-width-1, -width, -width+1, -1, +1, +width-1, +width, +width+1};
+		NEIGHBOURS9 = new int[]{-width-1, -width, -width+1, -1, 0, +1, +width-1, +width, +width+1};
 		
 		CIRCLE4 = new int[]{-width, +1, +width, -1};
 		CIRCLE8 = new int[]{-width-1, -width, -width+1, +1, +width+1, +width, +width-1, -1};
