26aeff9d163bd9d95b19b7c65482b6f127c1367c
juh9870 <juh9870@ukr.net>
2022-03-16 15:03:41 +0200

    Nerfed Legendary

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/curses/challenged/Legendary.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/curses/challenged/Legendary.java
@@ -2,11 +2,25 @@ package com.shatteredpixel.shatteredpixeldungeon.items.weapon.curses.challenged;
 
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.items.artifacts.Artifact;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.Weapon;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSprite;
 
 public class Legendary extends BuffEnchantment {
 	private static final ItemSprite.Glowing BLACK = new ItemSprite.Glowing( 0x000000 );
+
+	public static int strBonus( Hero hero ) {
+		if ( hero.buff( Weak.class ) == null ) return 0;
+
+		int penalty = -2;
+		int equipped = 0;
+		if ( hero.belongings.armor() != null ) equipped++;
+		if ( hero.belongings.artifact() != null ) equipped++;
+		if ( hero.belongings.misc() != null ) equipped++;
+		if ( hero.belongings.ring() != null ) equipped++;
+		penalty -= (equipped * (equipped + 1)) / 2;
+		return penalty;
+	}
 
 	@Override
 	public boolean curse() {
@@ -31,11 +45,6 @@ public class Legendary extends BuffEnchantment {
 	@Override
 	protected EnchantmentBuff buff() {
 		return new Weak();
-	}
-
-	public static int strBonus( Hero hero ) {
-		if ( hero.buff( Weak.class ) != null ) return -2;
-		return 0;
 	}
 
 	public class Weak extends EnchantmentBuff {
